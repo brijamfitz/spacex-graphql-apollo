cc335df049895da8d800a146de64ff4c6c2a972e
brijamfitz <brianjfitzgerald@gmail.com>
2021-03-31 19:01:16 -0400

    add basic logging plugin

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const { ApolloServer } = require('apollo-server');
 const { typeDefs } = require('./typeDefs');
 const { resolvers } = require('./resolvers');
+const { basicLogging } = require('./plugins');
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [basicLogging]
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
---
+++ src/plugins/index.js
@@ -0,0 +1,24 @@
+const basicLogging = {
+  requestDidStart(requestContext) {
+    const request = requestContext.request;
+
+    console.log("Request started");
+    console.log("Query: ", request.query);
+    console.log("Variables: ", request.variables);
+    
+    return {
+      didEncounterErrors(requestContext) {
+        console.log("An error occured in response to the query " + request.query);
+        console.log("Errors: ", requestContext.errors);
+      },
+
+      willSendResponse(requestContext) {
+        console.log("Response sent", requestContext.response);
+      },
+    };
+  },
+};
+
+module.exports = {
+  basicLogging,
+}
